a9ec70eff402171efde64b4ca4d0e98e86c8eb6b
Stanislao Vargas <e_vargas.v@outlook.com>
2022-09-23 13:52:22 -0400

    Bug N1 Arreglado table-client.js

--- Tables/table-client/table-client.js
+++ Tables/table-client/table-client.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded',function(event){
     if(!Boolean(sessionStorage.getItem("jwt"))){
         window.location.href = "../../login/login.html";
     }
-    let teams=[];
+    
     let status;
     const baseUrl='http://localhost:3030';
     
