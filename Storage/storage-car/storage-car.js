7d64bbaa3f3f5ededb0df613157a783e747504f0
Sebastian <sebastian.asturizaga@ucb.edu.bo>
2022-09-22 21:25:21 -0400

    Code Smell: eliminar comentario en storage-car.js

--- Storage/storage-car/storage-car.js
+++ Storage/storage-car/storage-car.js
@@ -5,7 +5,6 @@ window.addEventListener('DOMContentLoaded', function (event) {
     const baseUrl = 'http://localhost:3030';
     const url = `${baseUrl}/storage`;
     async function CarsStorage(e) {
-        // debugger
         let response = await fetch(url);
         try {
             if (response.status == 200) {
@@ -55,21 +54,9 @@ window.addEventListener('DOMContentLoaded', function (event) {
         let carid=this.dataset.buyClientCarId
         console.log("carid",carid)
         const url = `${baseUrl}/client/${clienteID}/Car/${carid}`;
-        // let response= await fetch(url);
-        // let data;
-        // try{
-        //     if(response.status==200){
-        //         data = await response.json();
-        //     }
-        // }
-        // catch{
-        //     alert("No se encontro al cliente")
-        // }
         fetch(url,{method: 'POST'}).then((res) => {
             console.log("res",res)
             if (res.status == 201) {
-                //data = await res.json()
-                //console.log("data",data)
                 alert("Compra exitosa")
                 window.location.reload()
             }
@@ -77,29 +64,8 @@ window.addEventListener('DOMContentLoaded', function (event) {
                 alert("No pudo completar la comprar")
             }
         })
-    
-        // if (window.confirm(`¿Esta seguro de eliminar el automovil ${data.middleName} ${data.lastName}?`)) {
-        //     //debugger
-        //     console.log(this);
-        //     console.log("this", this.dataset)
-        //     console.log("this", this.dataset.deleteClientId)
-        //     let clientId = this.dataset.deleteClientId
-        //     let url = `${baseUrl}/client/${clientId}`;
-        //     fetch(url, {
-        //         method: 'DELETE'
-        //     }).then((data) => {
-        //         if (data.status === 200) {
-        //             alert('El cliente fue eliminado');
-        //             fetchClients();
-        //         }
-        //         else {
-        //             alert("error no se pudo eliminar al cliente")
-        //         }
-        //     });
-        //}
     }
     function retornar(){
-        //debugger
         let wls=window.location.search
         let urlO=`http://127.0.0.1:5500/views/view-client/view-client.html`+wls
         window.location.href=urlO
